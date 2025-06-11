63d5a18ecadb2511e02c550be330f1b0a2214492
Sue Smith <sue@benormal.info>
2025-06-11 11:31:46 +0000

    dir move

--- vite.config.js
+++ vite.config.js
@@ -3,37 +3,10 @@ import { defineConfig } from "vite";
 import handlebars from "vite-plugin-handlebars";
 import settings from "./settings.json";
 
-// Read the glitch assets file
-/*
-const fs = require("fs");
-const data = fs
-  .readFileSync("./.glitch-assets")
-  .toString("UTF8")
-  .split("\n")
-  .filter((l) => l.length > 0)
-  .map((l) => JSON.parse(l));
-// Get undeleted pics
-const pics = data.filter(
-  (img) => img && img.deleted == undefined && img.type.indexOf("image") >= 0
-);
-// We won't display pics marked as deleted
-const deleted = data.filter((img) => img && img.deleted).map((d) => d.uuid);
-*/
-// We also include any pics added in settings json
+// We include any pics added in settings json
 let pics = settings.pics;
-/*added.forEach((a) => {
-  let lastSlash = a.url.lastIndexOf("/");
-  a.thumbnail =
-    a.url.substring(0, lastSlash + 1) +
-    "thumbnails%2F" +
-    a.url.substring(lastSlash + 1);
-  a.name = a.url.substring(lastSlash + 1).split("?")[0];
-});*/
-//let pics = [];
-//pics.push(...added);
 // Show most recent first
 let assets = pics
-//  .filter((img) => !deleted.includes(img.uuid))
   .sort((a, b) => {
     if (a.date > b.date) {
       return -1;
@@ -62,7 +35,7 @@ export default defineConfig({
   ],
   build: {
     cssCodeSplit: false,
-    outDir: "build",
+    outDir: "docs",
   },
   server: {
     host: "0.0.0.0",
